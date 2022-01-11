27635606107a9335da5dce5a82e2c670407ed03a
MRIkaito <pnfb21dp@s.okayama-u.ac.jp>
2022-01-11 13:33:34 +0900

    ペジネーション機能追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index(Post $post) //「PostモデルにあるPostインスタンスの性質を持つ」変数$postという意味
     {
-    return view('posts/index') -> with(['posts' => $post->getByLimit()]);
+    return view('posts/index') -> with(['posts' => $post->getPaginateByLimit()]);
     }
     // Post.phpにある$postがgetByLimit()関数によって得た返り値を，
     // $postsという変数名で格納して，posts/indexに表示した．
--- app/Post.php
+++ app/Post.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public function getByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 5)
     {
-        // updated_atで降順に並べたあと、limitで件数制限をかける
-        return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+        // updated_atで降順(DESC)に並べたあと、peginateで表示件数に制限をかける
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 }
